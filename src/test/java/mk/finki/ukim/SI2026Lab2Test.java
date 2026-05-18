385319665e5e7293699daf0eb1ec44ec1b2cc320
Mario Dimeski <mariodimeski03@gmail.com>
2026-05-18 09:35:09 +0200

    minor changes

--- src/test/java/mk/finki/ukim/SI2026Lab2Test.java
+++ src/test/java/mk/finki/ukim/SI2026Lab2Test.java
@@ -13,33 +13,33 @@ class SI2026Lab2Test {
     @Test
     void searchBookEveryStatementTest()
     {
-//        Library library = new Library();
-//        Library library2 = new Library();
-//
-//        library.addBook(new Book("Clean Code","Robert C. Martin","Programming"));
-//
-//        /**
-//         * Case 1: empty title -> expected exception
-//         */
-//        assertThrows(IllegalArgumentException.class, () -> library.searchBookByTitle(""));
-//
-//        /**
-//         * Case 2: searching for 1 book -> expected book to be found
-//         */
-//        List<Book> bookList = library.searchBookByTitle("Clean Code");
-//        assertEquals("Clean Code", bookList.get(0).getTitle());
-//
-//        /**
-//         * Case 3: Empty library with no books -> expected null
-//         */
-//        List<Book> bookList2 = library2.searchBookByTitle("Clean Code");
-//        assertNull(bookList2);
-//
-//        /**
-//         * Case 4: Searching for a book that doesn't exist -> expected null
-//         */
-//        List<Book> bookList3 = library.searchBookByTitle("Random book");
-//        assertNull(bookList3);
+        Library library = new Library();
+        Library library2 = new Library();
+
+        library.addBook(new Book("Clean Code","Robert C. Martin","Programming"));
+
+        /**
+         * Case 1: empty title -> expected exception
+         */
+        assertThrows(IllegalArgumentException.class, () -> library.searchBookByTitle(""));
+
+        /**
+         * Case 2: searching for 1 book -> expected book to be found
+         */
+        List<Book> bookList = library.searchBookByTitle("Clean Code");
+        assertEquals("Clean Code", bookList.get(0).getTitle());
+
+        /**
+         * Case 3: Empty library with no books -> expected null
+         */
+        List<Book> bookList2 = library2.searchBookByTitle("Clean Code");
+        assertNull(bookList2);
+
+        /**
+         * Case 4: Searching for a book that doesn't exist -> expected null
+         */
+        List<Book> bookList3 = library.searchBookByTitle("Random book");
+        assertNull(bookList3);
     }
 
     @Test
