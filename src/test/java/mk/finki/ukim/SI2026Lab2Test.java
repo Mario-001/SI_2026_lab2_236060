b20f45349d6da3a5518037dc769b393cb609e10b
Mario Dimeski <mariodimeski03@gmail.com>
2026-05-18 09:33:29 +0200

    borrowBookEveryBranchTest

--- src/test/java/mk/finki/ukim/SI2026Lab2Test.java
+++ src/test/java/mk/finki/ukim/SI2026Lab2Test.java
@@ -1,7 +1,9 @@
 package mk.finki.ukim;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.ref.Cleaner;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,38 +13,49 @@ class SI2026Lab2Test {
     @Test
     void searchBookEveryStatementTest()
     {
-        Library library = new Library();
-        Library library2 = new Library();
-
-        library.addBook(new Book("Clean Code","Robert C. Martin","Programming"));
-
-        /**
-         * Case 1: empty title -> expected exception
-         */
-        assertThrows(IllegalArgumentException.class, () -> library.searchBookByTitle(""));
-
-        /**
-         * Case 2: searching for 1 book -> expected book to be found
-         */
-        List<Book> bookList = library.searchBookByTitle("Clean Code");
-        assertEquals("Clean Code", bookList.get(0).getTitle());
-
-        /**
-         * Case 3: Empty library with no books -> expected null
-         */
-        List<Book> bookList2 = library2.searchBookByTitle("Clean Code");
-        assertNull(bookList2);
-
-        /**
-         * Case 3: Searching for a book that doesn't exist -> expected null
-         */
-        List<Book> bookList3 = library.searchBookByTitle("Random book");
-        assertNull(bookList3);
+//        Library library = new Library();
+//        Library library2 = new Library();
+//
+//        library.addBook(new Book("Clean Code","Robert C. Martin","Programming"));
+//
+//        /**
+//         * Case 1: empty title -> expected exception
+//         */
+//        assertThrows(IllegalArgumentException.class, () -> library.searchBookByTitle(""));
+//
+//        /**
+//         * Case 2: searching for 1 book -> expected book to be found
+//         */
+//        List<Book> bookList = library.searchBookByTitle("Clean Code");
+//        assertEquals("Clean Code", bookList.get(0).getTitle());
+//
+//        /**
+//         * Case 3: Empty library with no books -> expected null
+//         */
+//        List<Book> bookList2 = library2.searchBookByTitle("Clean Code");
+//        assertNull(bookList2);
+//
+//        /**
+//         * Case 4: Searching for a book that doesn't exist -> expected null
+//         */
+//        List<Book> bookList3 = library.searchBookByTitle("Random book");
+//        assertNull(bookList3);
     }
 
     @Test
-    void borrowBook()
+    void borrowBookEveryBranchTest()
     {
+        Library library = new Library();
+        library.addBook(new Book("Clean Code","Robert C. Martin","Programming"));
 
+        assertThrows(IllegalArgumentException.class,() -> library.borrowBook("",""));
+
+        library.borrowBook("Clean Code", "Robert C. Martin");
+
+        assertNull(library.searchBookByTitle("Clean Code"));
+
+        assertThrows(RuntimeException.class, () -> library.borrowBook("xyz","xyz"));
+
+        assertThrows(RuntimeException.class,() -> library.borrowBook("Clean Code","Robert C. Martin"));
     }
 }
